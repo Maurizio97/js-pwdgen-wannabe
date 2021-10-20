cb6638852b716b188eb9b73a04d94732a8aa334e
Maurizio <proiettomaurizio97@gmail.com>
2021-10-20 15:27:16 +0200

    bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,40 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21  */
+
+
+/* // i ask the user for his name "chiedo all'utente il suo nome"
+const name = prompt("Dimmi il tuo nome")
+
+// i ask the user for his surname "chiedo all'utente il suo cognome"
+const surname = prompt("Dimmi il tuo cognome")
+
+// i ask for his favorite color "chiedo il suo colore preferito"
+const favoriteColor = prompt("Dimmi il tuo colore preferito")
+
+// i write on the page "la password assegnata é: " namesurnamefavoritecolor21
+let password = "La password assegnata é:"
+document.getElementById("generatore-password")
+.innerHTML= `${name}${surname}${favoriteColor}21`
+ */
+ 
+
+// i ask the user for his name "chiedo all'utente il suo nome"
+let name;
+document.getElementById(name)
+
+// i ask the user for his surname "chiedo all'utente il suo cognome"
+const surname;
+document.getElementById(surname)
+
+
+// i ask for his favorite color "chiedo il suo colore preferito"
+const favoriteColor;
+document.getElementById(favoritecolor)
+
+
+// i write on the page "la password assegnata é: " namesurnamefavoritecolor21
+let password = "La password assegnata é:"
+document.getElementById("generatore-password")
+.innerHTML= `${name}${surname}${favoriteColor}21`
