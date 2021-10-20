d59dd35724617c8c3616d2a6a7294b37a9941a5a
Maurizio <proiettomaurizio97@gmail.com>
2021-10-20 15:04:49 +0200

    i ask the user for his name complete

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21  */
+
+// i ask the user for his name "chiedo all'utente il suo nome"
+const name = prompt("Dimmi il tuo nome")
+// i ask the user for his surname "chiedo all'utente il suo cognome"
+const surname
+// i ask for his favorite color "chiedo il suo colore preferito"
+
+// i write on the page "la password assegnata é: " namesurnamefavoritecolor21
+let password = "La password assegnata é:"
+document.getElementById("generatore-password")
+.innerHTML= `${name}21`
